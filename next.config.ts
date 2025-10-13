ae3ef846bbe4f30cae427417b927dfff9d114ae4
MVRCreative <154540859+MVRCreative@users.noreply.github.com>
2025-10-13 13:21:57 -0400

    webflow cloud changes

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*.supabase.co',
-        port: '',
-        pathname: '/storage/v1/object/public/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  basePath: "/app", // your environment's mount path
+  assetPrefix: "/app", // ensure this matches your environment's mount path
 };
 
 export default nextConfig;
+
+// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
